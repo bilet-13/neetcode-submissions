10b755fb2648b3d755c2c521d4717a3517ad6ff9
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-03-30 11:51:52 -0700

    Add: course-schedule-iv - submission-6

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-6.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        # uj is a prerequisite of vj equal to there is a path from uj to vj
+        # node: course edge: uj to vj if uj is a prerequisite
+        # so for each query, use dfs or bfs check if there is a path
+        # tiem complexity o(len(queries) * (V + E)) = o(len(queries) * (numcourses + len(prereqruisite)))
+
+        # udpate version:
+        # dp -> floyd warshl o(V^3)
+        # hash map of set to sotre all prereq for course i X
+
+        dp = [[False for _ in range(numCourses)] for _ in range(numCourses)]
+
+        for a, b in prerequisites:
+            dp[a][b] = True
+
+        for k in range(numCourses):
+            for i in range(numCourses):
+                for j in range(numCourses):
+                    dp[i][j] |= dp[i][k] & dp[k][j]
+        
+        result = []
+
+        for u, v in queries:
+            result.append(dp[u][v])
+        
+        return result
+
+
