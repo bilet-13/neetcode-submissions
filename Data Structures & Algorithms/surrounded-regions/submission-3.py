4b45ea05a8cc52bf57a3fa3f299cdcb54acfbd44
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-04-01 22:22:30 -0700

    Add: surrounded-regions - submission-3

---
+++ Data Structures & Algorithms/surrounded-regions/submission-3.py
@@ -0,0 +1,40 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        # not surround region -> the groupd that has 'O' on an edge
+        # so change regions that don't have o on an edge
+        # dfs for  
+        
+        def dfs(start_x, start_y):
+            stack = [(start_x, start_y)]
+            visited = set(stack)
+
+            n = len(board)
+            m = len(board[0])
+            surrounded = True
+
+            while stack:
+                cur_x, cur_y = stack.pop()
+
+                if cur_x == 0 or cur_x == n - 1 or cur_y == 0 or cur_y == m - 1:
+                    surrounded = False
+                
+                for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    nx = cur_x + dx
+                    ny = cur_y + dy
+
+                    if n > nx >= 0 and m > ny >= 0 and board[nx][ny] == 'O' and (nx, ny) not in visited:
+                        visited.add((nx, ny))
+                        stack.append((nx, ny))
+
+            if surrounded:
+                for x, y in visited:
+                    board[x][y] = 'X'
+
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == 'O':
+                    dfs(i, j)
+
+        return
+        
