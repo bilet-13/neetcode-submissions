a2f08215b3411de37168c653bea55e6d243a2a61
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-05-04 12:16:56 -0700

    Add: min-cost-to-connect-points - submission-11

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-11.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        # minimum spanning tree
+        # prim
+        # dense graph -> prim
+
+        visited = set() 
+        min_heap = [(0, 0)]
+        min_cost = 0
+
+        while min_heap:
+            dist, u = heapq.heappop(min_heap)
+
+            if u in visited:
+                continue
+
+            visited.add(u)
+            min_cost += dist
+
+            for i in range(len(points)):
+                if i in visited:
+                    continue
+                n_dist = abs(points[u][0] - points[i][0]) + abs(points[u][1] - points[i][1])
+                heapq.heappush(min_heap, (n_dist, i))
+        
+        return min_cost
+
+
+            
+
+
+
+
+        
