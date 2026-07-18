cf4a737fb25c37830315b29e153d32155d90493b
Chun-Wei <40483596+bilet-13@users.noreply.github.com>
2026-07-18 15:42:30 -0700

    Add: last-stone-weight-ii - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight-ii/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+         #smallest
+        n = len(stones)
+        cap = sum(stones) // 2
+        dp = [0 for _ in range(cap + 1)]
+
+        for i in range(n):
+            for c in range(cap, -1, -1):
+                if c - stones[i] >= 0:
+                    dp[c] = max(dp[c], dp[c - stones[i]] + stones[i])
+
+        return sum(stones) - (2 * dp[cap]) 
+
+
+
+       
+
+
+         
